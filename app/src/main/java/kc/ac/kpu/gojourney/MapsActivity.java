a3d134765ff75f3274563dcc60912e381a6f8e4c
HyunbinKang <53068928+HyunbinKang@users.noreply.github.com>
2020-03-24 20:39:57 +0900

    start

---
+++ app/src/main/java/kc/ac/kpu/gojourney/MapsActivity.java
@@ -0,0 +1,66 @@
+package kc.ac.kpu.gojourney;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        //google map이 준비가 됐다
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        // 위도 경도 표현하는 객체
+        LatLng murcia = new LatLng(37.9922399, -1.1306544);
+        mMap.addMarker(new MarkerOptions().position(murcia).title("무르시아다히히"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(murcia));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(MapsActivity.this, MurciaActivity.class);
+                        startActivity(intent);
+                       // finish();
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/kc/ac/kpu/gojourney/MurciaActivity.java
@@ -0,0 +1,21 @@
+package kc.ac.kpu.gojourney;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MurciaActivity extends AppCompatActivity {
+   // ImageView ImageView_get;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_murcia);
+
+       // ImageView_get = findViewById(R.id.imageView2);
+
+        Intent intent = getIntent();
+    }
+}
